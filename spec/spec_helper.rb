17bc0f59276542245beb1e32805d7c8b33d7d765
amay22 <amay@nyu.edu>
2015-06-30 13:00:12 -0400

    figs authentication added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,4 @@
-require File.expand_path('../../lib/loaders/base.rb', __FILE__)
-require File.expand_path('../../lib/loaders/empty_theme.rb', __FILE__)
-require File.expand_path('../../lib/loaders/utilities/google_sheet.rb', __FILE__)
-require File.expand_path('../../lib/loaders/helpers/department.rb', __FILE__)
+require File.expand_path('../../lib/siteleaf.rb', __FILE__)
 require 'vcr'
 require 'webmock/rspec'
 
@@ -9,9 +6,6 @@ VCR.configure do |config|
   config.default_cassette_options = { record: :new_episodes, match_requests_on: [:path] }
   config.cassette_library_dir     = 'spec/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('KEY') { ENV['SITELEAF_KEY'] } unless ENV['SITELEAF_KEY'].nil?
-  config.filter_sensitive_data('SECRET') { ENV['SITELEAF_SECRET'] } unless ENV['SITELEAF_KEY'].nil?
-  config.filter_sensitive_data('ID') { ENV['SITELEAF_ID'] } unless ENV['SITELEAF_ID'].nil?
   config.configure_rspec_metadata!
   config.debug_logger = File.open('vcr_errors', 'w')
 end
